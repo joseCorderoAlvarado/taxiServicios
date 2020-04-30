b50d648c31f355698b9e86a66629e9978a3abf38
Jose Cordero <careliapatino@gmail.com>
2020-04-30 13:32:58 -0600

    validaciones para el registro de nuevo usuario

--- taxiServicios/app/src/main/java/com/example/taxiservicios/registro.java
+++ taxiServicios/app/src/main/java/com/example/taxiservicios/registro.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,7 +29,7 @@ public class registro extends AppCompatActivity {
  EditText txtNombre,txtTelefono,txtcorreo,txtContrasena;
  Button btnregistro;
  TextView iniciarsesion;
-    String correo,contrasena,nombre,telefono;
+    String correo,contrasena,nombre,telefono,correov;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,15 +41,22 @@ public class registro extends AppCompatActivity {
         btnregistro=findViewById(R.id.btnregistro);
         iniciarsesion=findViewById(R.id.viewiniciarsesion);
         recuperarpreferencias();
+
         btnregistro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-             correo=txtcorreo.getText().toString();
-             contrasena=txtContrasena.getText().toString();
+                correo=txtcorreo.getText().toString();
+                if (Patterns.EMAIL_ADDRESS.matcher(correo).matches()) {
+                    correov = correo;
+                } else {
+                    Toast.makeText(registro.this,"Este no es un correo eléctronico \n ejemplo@gmail.com ",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                contrasena=txtContrasena.getText().toString();
              nombre=txtNombre.getText().toString();
              telefono=txtTelefono.getText().toString();
              if(!correo.isEmpty() && !contrasena.isEmpty() && !nombre.isEmpty() && !telefono.isEmpty()){
-                 registrarUsuario("http://pruebataxi.laviveshop.com/app/registro.php");
+                 validarRegistro("http://pruebataxi.laviveshop.com/app/validarRegistro.php");
              }
              else
                  {
@@ -55,6 +64,38 @@ public class registro extends AppCompatActivity {
                  }
             }
         });
+    }
+    private void  validarRegistro(String URL)
+    {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("valor", response);
+             if(response.isEmpty())
+             {
+                 Toast.makeText(registro.this,"Este usuario ya se encuentra registrado",Toast.LENGTH_SHORT).show();
+             }
+             else
+                 {
+                     registrarUsuario("http://pruebataxi.laviveshop.com/app/registro.php");
+                 }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(registro.this,error.toString(),Toast.LENGTH_SHORT).show();
+            }
+        })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> parametros = new HashMap<String, String>();
+                parametros.put("correo",correov);
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
     private void registrarUsuario(String URL)
     {
@@ -78,7 +119,7 @@ public class registro extends AppCompatActivity {
                 Map<String,String> parametros = new HashMap<String, String>();
                 parametros.put("nombre",nombre);
                 parametros.put("telefono",telefono);
-                parametros.put("correo",correo);
+                parametros.put("correo",correov);
                 parametros.put("contrasena",contrasena);
                 return parametros;
             }
@@ -90,7 +131,7 @@ public class registro extends AppCompatActivity {
     {
         SharedPreferences preferences=getSharedPreferences("preferenciasLogin", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor =preferences.edit();
-        editor.putString("correo",correo);
+        editor.putString("correo",correov);
         editor.putString("contrasena",contrasena);
         editor.putInt("tipo",2);
         editor.putBoolean("sesion",true);
