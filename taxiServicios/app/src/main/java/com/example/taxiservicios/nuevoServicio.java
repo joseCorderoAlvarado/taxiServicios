a3ad71af830d05b880e9ee25ee17253d565a78ed
Jose Cordero <careliapatino@gmail.com>
2020-05-02 11:31:26 -0600

    interfaz de nuevo servicio sin funcionalidad

--- taxiServicios/app/src/main/java/com/example/taxiservicios/nuevoServicio.java
+++ taxiServicios/app/src/main/java/com/example/taxiservicios/nuevoServicio.java
@@ -1,15 +1,25 @@
 package com.example.taxiservicios;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
+import static android.app.Activity.RESULT_OK;
+
 public class nuevoServicio extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
