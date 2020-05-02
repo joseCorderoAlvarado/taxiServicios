142b2f7eb0a7a146807d134e2d25f454e9fab313
Jose Cordero <careliapatino@gmail.com>
2020-05-02 15:32:29 -0600

    cambios a el tolbar del cliente y adicion a la referencia de complementos

--- taxiServicios/app/src/main/java/com/example/taxiservicios/nuevoServicio.java
+++ taxiServicios/app/src/main/java/com/example/taxiservicios/nuevoServicio.java
@@ -5,7 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,11 +24,29 @@ import com.google.android.gms.location.places.ui.PlacePicker;
 import static android.app.Activity.RESULT_OK;
 
 public class nuevoServicio extends Fragment {
-
+DatePicker fecha;
+TimePicker hora;
+Spinner sOrigen, sDestino;
+EditText txtOrigen, txtDestino,txtcomentarios;
+Button btnNuevo;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.fragment_nuevoservicio,container,false);
+        fecha=view.findViewById(R.id.dpfecha);
+        hora=view.findViewById(R.id.tphora);
+        sOrigen=view.findViewById(R.id.spinnerpartida);
+        sDestino=view.findViewById(R.id.spinnerdestino);
+        txtOrigen=view.findViewById(R.id.txtOrigen);
+        txtDestino=view.findViewById(R.id.txtDestino);
+        txtcomentarios=view.findViewById(R.id.txtComentarios);
+        btnNuevo=view.findViewById(R.id.btnnuevo);
+        btnNuevo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
         return view;
     }
 }
