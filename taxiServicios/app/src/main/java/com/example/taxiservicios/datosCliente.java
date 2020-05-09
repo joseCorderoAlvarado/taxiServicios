229bf66750e7f64495cf7d4b5b3207f95e960be8
Jose Cordero <careliapatino@gmail.com>
2020-05-08 22:33:21 -0600

    datos cliente

--- taxiServicios/app/src/main/java/com/example/taxiservicios/datosCliente.java
+++ taxiServicios/app/src/main/java/com/example/taxiservicios/datosCliente.java
@@ -1,19 +1,122 @@
 package com.example.taxiservicios;
-
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.HashMap;
+import java.util.Map;
 public class datosCliente extends Fragment {
+    EditText txtNombre,txtTelefono,txtContraseña;
+    Button btnmodificar;
+    String correo, nombre,telefon,contrasena;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.fragment_datoscliente,container,false);
+        txtNombre=view.findViewById(R.id.txtNombreC);
+        txtContraseña=view.findViewById(R.id.txtContrasenaC);
+        txtTelefono=view.findViewById(R.id.txtTelefonoC);
+        btnmodificar=view.findViewById(R.id.btnModificarC);
+        SharedPreferences preferences = getActivity().getSharedPreferences("preferenciasLogin", Context.MODE_PRIVATE);
+        correo=preferences.getString("correo",null);
+        cargardatos("http://pruebataxi.laviveshop.com/app/consultardatoscliente.php",correo);
+        btnmodificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                telefon=txtTelefono.getText().toString();
+                contrasena=txtContraseña.getText().toString();
+                nombre=txtNombre.getText().toString();
+                modificardatos("http://pruebataxi.laviveshop.com/app/actualizardatoscliente.php",nombre,telefon,contrasena,correo);
+                homeCliente home = new homeCliente();
+                AppCompatActivity activity = (AppCompatActivity) v.getContext();
+                activity.getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment, home).addToBackStack(null).commit();
+            }
+        });
         return  view;
+    }
+    private  void cargardatos(String URL, final String correoobt)
+    {
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject valores = new JSONObject(response);
+                    JSONArray jsonArray=valores.getJSONArray("datos");
+                    for(int i=0;i<jsonArray.length();i++)
+                    {
+                        JSONObject jsonObject=jsonArray.getJSONObject(i);
+                        String nombre =jsonObject.getString("nombre");
+                        String telefono =jsonObject.getString("telefono");
+                        txtTelefono.setText(telefono);
+                        txtNombre.setText(nombre);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getActivity().getBaseContext(),error.toString(),Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> parametros = new HashMap<String, String>();
+                parametros.put("correo",correoobt);
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue= Volley.newRequestQueue(getActivity().getBaseContext());
+        requestQueue.add(stringRequest);
+    }
+    private  void  modificardatos(String URL, final String nombreobt, final String telefonoobt, final String contrasenaobt, final String correoobt)
+    {
+        StringRequest stringRequest= new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getActivity().getBaseContext(),"Datos actualizados con exito!!",Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getActivity().getBaseContext(),"Error al modificar los datos",Toast.LENGTH_SHORT).show();
+            }
+        })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> parametros = new HashMap<String, String>();
+                parametros.put("nombre",nombreobt.toString());
+                parametros.put("telefono",telefonoobt.toString());
+                parametros.put("contrasena",contrasenaobt.toString());
+                parametros.put("correo",correoobt.toString());
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue= Volley.newRequestQueue(getActivity().getBaseContext());
+        requestQueue.add(stringRequest);
     }
 }
