d43b296f596e11cf5baacde8f65b13c4894ee61f
Ramon Diaz <ramondiaz123@yahoo.com>
2020-05-16 20:54:23 -0600

    Notifiaciones tambien en Foreground

--- taxiServicios/app/src/main/java/com/example/taxiservicios/MyFirebaseMessagingService.java
+++ taxiServicios/app/src/main/java/com/example/taxiservicios/MyFirebaseMessagingService.java
@@ -1,10 +1,18 @@
 package com.example.taxiservicios;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -18,6 +26,36 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     }
 
     private static String deviceToken;
+
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        sendNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+    }
+
+    private void sendNotification(String messageTitle,String messageBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0 /* request code */, intent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        long[] pattern = {500,500,500,500,500};
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_contact_mail_black_24dp)
+                .setContentTitle(messageTitle)
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setVibrate(pattern)
+                .setLights(Color.BLUE,1,1)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+
 
     /**
      * Called if InstanceID token is updated. This may occur if the security of
