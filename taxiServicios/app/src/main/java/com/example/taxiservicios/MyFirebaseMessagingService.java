20ad4a2b9ddbfc1dc2d717e48aa8c36af6efb0e3
Ramon Diaz <ramondiaz123@yahoo.com>
2020-05-21 17:10:23 -0600

    Notificacions para android 8 y mayor v1

--- taxiServicios/app/src/main/java/com/example/taxiservicios/MyFirebaseMessagingService.java
+++ taxiServicios/app/src/main/java/com/example/taxiservicios/MyFirebaseMessagingService.java
@@ -1,9 +1,10 @@
 package com.example.taxiservicios;
 
-import android.app.Notification;
+
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -15,11 +16,14 @@ import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Random;
 
 import static com.google.android.gms.plus.internal.PlusCommonExtras.TAG;
 
@@ -31,30 +35,10 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static String deviceToken;
 
+    private NotificationManager notificationManager;
+    private static final String ADMIN_CHANNEL_ID ="admin_channel";
+
     Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-
-    @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
-        super.onMessageReceived(remoteMessage);
-        Log.d("msg", "El mensaje es: " + remoteMessage.getData().get("body"));
-        Notification notification = new NotificationCompat.Builder(this)
-                .setContentTitle(remoteMessage.getData().get("title"))
-                .setContentText(remoteMessage.getData().get("body"))
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setSound(defaultSoundUri)
-                .build();
-        NotificationManagerCompat manager = NotificationManagerCompat.from(getApplicationContext());
-        manager.notify(123, notification);
-    }
-
-
-
-
-
-
-
-
-
 
 
     /**
@@ -73,6 +57,56 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // i.e. store it on SharedPreferences or DB
         // or directly send it to server
 
+
+    }
+
+
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // En este método recibimos el mensaje
+        Intent notificationIntent;
+        notificationIntent = new Intent(this, inicioAdministrador.class);
+
+
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        final PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent,
+                PendingIntent.FLAG_ONE_SHOT);
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        // Configuramos la notificación para Android Oreo o superior
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            setupChannels();
+        }
+        int notificationId = new Random().nextInt(60000);
+        // Creamos la notificación en si
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, ADMIN_CHANNEL_ID)
+                .setSmallIcon(R.drawable.logow)  //a resource for your custom small icon
+                .setContentTitle(remoteMessage.getData().get("title")) //the "title" value you sent in your notification
+                .setContentText(remoteMessage.getData().get("body")) //ditto
+                .setAutoCancel(true)  //dismisses the notification on click
+                .setContentIntent(pendingIntent)
+                .setSound(defaultSoundUri);
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(notificationId /* ID of notification */, notificationBuilder.build());
+    }
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void setupChannels() {
+        CharSequence adminChannelName = "Administrador";
+        String adminChannelDescription = "Canal del Admin";
+        NotificationChannel adminChannel;
+        adminChannel = new NotificationChannel(ADMIN_CHANNEL_ID, adminChannelName, NotificationManager.IMPORTANCE_HIGH);
+        adminChannel.setDescription(adminChannelDescription);
+        adminChannel.enableLights(true);
+        adminChannel.setLightColor(Color.RED);
+        adminChannel.enableVibration(true);
+        if (notificationManager != null) {
+            notificationManager.createNotificationChannel(adminChannel);
+        }
 
     }
 }
